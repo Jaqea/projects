c0ad384bdb9533cdf2ad008734a422854dd29255
Jaqea <1601676692@qq.com>
2020-02-27 21:26:12 +0800

    第三次作业提交

--- js/main.js
+++ js/main.js
@@ -1,70 +1,31 @@
 window.onload = function() {
-    var oBtn = document.querySelector('.btn');
     var oTxt = document.querySelector('.txt');
-    var oDiv2 = document.querySelector('.div2');
-    var aLi = oDiv2.querySelectorAll('li');
-    var randNum = Math.ceil(100 * Math.random());
-    var count = 0;
-    oBtn.onclick = guess;
+    var oBtn = document.querySelector('.btn');
+    var oDiv3 = document.querySelector('.div3');
+    var arr1 = ['怪兽威利', '大老爹', '圣诞老人'];
+    var arr2 = ['迪士尼乐园', '救助站', '白宫'];
+    var arr3 = ['在人行道化成了一坨泥', '自燃了', '变成一条鼻涕虫爬走了'];
+    var arr = [];
+
+    oBtn.onclick = generated;
     oTxt.onkeydown = function(e) {
         if (e.keyCode == 13)
-            guess()
-        else return
+            generated();
     }
 
-    function guess() {
-        if (isNaN(parseInt(oTxt.value))) {
-            alert('请输入一个0~100以内的数字！');
-        } else {
-            count++;
-            if (count == 10) {
-                oTxt.disabled = true;
-                oBtn.disabled = true;
-                oBtn.className = 'btn btn1';
-            }
-            oDiv2.style.display = 'block';
-            if (oTxt.value != randNum) {
-                oTxt.value > randNum ? aLi[2].innerText = '刚才你猜高了' : aLi[2].innerText = '刚才你猜低了';
-                aLi[0].innerText += oTxt.value + '\xa0';
-                if (count == 10) {
-                    aLi[1].innerText = '!!!GAME OVER!!!';
-                    createBtn();
-                } else
-                    aLi[1].innerText = '很遗憾，你猜错了';
-                aLi[1].className = 'error';
-                oTxt.value = '';
-            } else {
-                aLi[1].innerText = '恭喜你，猜对了';
-                aLi[1].className = 'success';
-                aLi[2].innerText = '';
-                count = 0;
-                oTxt.disabled = true;
-                oBtn.disabled = true;
-                oBtn.className = 'btn btn1';
-                createBtn();
-            }
-        }
+    function random(arr) {
+        var index = Math.floor(arr.length * Math.random());
+        return arr[index];
     }
 
-    function createBtn() {
-        var newBtn = document.createElement('input');
-        var newLi = document.createElement('li');
-        var oUl = oDiv2.querySelector('ul');
-        newBtn.type = 'button';
-        newBtn.className = 'btn';
-        newBtn.style.marginLeft = '0px';
-        newBtn.value = '开始新游戏';
-        newBtn.onclick = function() {
-            oTxt.disabled = false;
-            oBtn.disabled = false;
-            aLi[0].innerText = '上次猜的数：';
-            oBtn.className = 'btn';
-            oDiv2.style.display = 'none';
-            oUl.removeChild(newLi);
-            oTxt.value = '';
-            randNum = Math.ceil(100 * Math.random());
-        }
-        newLi.appendChild(newBtn);
-        oUl.appendChild(newLi);
+    function generated() {
+        if (arr) arr = [];
+        arr.push(random(arr1));
+        arr.push(random(arr2));
+        arr.push(random(arr3));
+        if (!oTxt.value)
+            name = '李雷';
+        else name = oTxt.value;
+        oDiv3.innerText = '外边有34度，' + arr[0] + '出去遛弯。当走到' + arr[1] + '时小伙伴们都惊呆了，他' + arr[2] + '。' + name + '全程目睹但他并没有慌，因为' + arr[0] + '是一个270斤的胖子，天气又辣么热。';
     }
 }
